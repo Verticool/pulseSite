6ad73d8a9b0f19448d3d76fa898f97d4c35d2134
Verticool <darkeroman@gmail.com>
2022-10-13 22:23:31 +0600

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,15 @@
 var slider = tns({
-    container: '.carousel',
+    container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
-    autoplay: true
+    autoplay: true,
+    controls: false,
+    nav: false,
+    autoplayButtonOutput: false
 });
+document.querySelector('.prev').onclick = function () {
+  slider.goTo('prev');
+};
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+  };
