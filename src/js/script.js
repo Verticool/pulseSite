dc8eda7f1863c34274e11f2f99a96b40d3e41ade
Verticool <darkeroman@gmail.com>
2022-10-14 20:04:22 +0600

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,26 @@
 var slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: true,
-    controls: false,
-    nav: false,
-    autoplayButtonOutput: false
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  controls: false,
+  nav: false,
+  autoplayButtonOutput: false
 });
 document.querySelector('.prev').onclick = function () {
   slider.goTo('prev');
 };
 document.querySelector('.next').onclick = function () {
-    slider.goTo('next');
-  };
+  slider.goTo('next');
+};
+(function ($) {
+  $(function () {
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+  });
+})(jQuery);
